05a4fdbffd4a3c5396629b18c211b76c31ec4bba
Tim <vogelti64073@th-nuernberg.de>
2016-05-11 20:53:43 +0200

    moved bluetooth transfer in background thread. Display Dialog to show progress during transfer of data

--- app/src/main/java/de/lddt/zeichenroboterapp/MainActivity.java
+++ app/src/main/java/de/lddt/zeichenroboterapp/MainActivity.java
@@ -1,6 +1,8 @@
 package de.lddt.zeichenroboterapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -52,30 +54,70 @@ public class MainActivity extends Activity {
             Toast.makeText(MainActivity.this, getString(R.string.nothing_drawn), Toast.LENGTH_SHORT).show();
             return;
         }
+
         if (!BluetoothConn.isBluetoothEnabled()) {
             Toast.makeText(MainActivity.this, getString(R.string.bluetooth_disabled), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        boolean succesfullyConnected = BluetoothConn.connectTo(
-                new MyBrick(getString(R.string.brick_name), getString(R.string.brick_mac_address)));
-        if (!succesfullyConnected) {
-            Toast.makeText(MainActivity.this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        boolean succesfullySend = sendData(directionVectorList);
-        if (succesfullySend) {
-            Toast.makeText(MainActivity.this, "Daten gesendet!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(MainActivity.this, getString(R.string.data_transfer_failed), Toast.LENGTH_SHORT).show();
-        }
+        new TransferVectorsToBrick().execute(directionVectorList);
 
         BluetoothConn.close();
     }
 
-    private boolean sendData(List<Vector2D> vectorList) {
-        byte[] data = ToByteConverter.vectorListToByte(vectorList);
-        return BluetoothConn.send(data);
+    private class TransferVectorsToBrick extends AsyncTask<List<Vector2D>, String, String> {
+        private final AlertDialog dialog;
+
+        public TransferVectorsToBrick() {
+            this.dialog = createDialog(getString(R.string.connect_dialog_title), getString(R.string.connect_dialog_message));
+            this.dialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(List<Vector2D>... params) {
+            boolean succesfullyConnected = BluetoothConn.connectTo(
+                    new MyBrick(getString(R.string.brick_name), getString(R.string.brick_mac_address)));
+
+            if (!succesfullyConnected) {
+                return getString(R.string.connection_failed);
+            }
+
+            publishProgress(getString(R.string.send_dialog_title), getString(R.string.send_dialog_message));
+            boolean succesfullySend = sendData(params[0]);
+
+            if (succesfullySend) {
+                return getString(R.string.data_transfer_success);
+            }
+            return getString(R.string.data_transfer_failed);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+            dialog.cancel();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog.show();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            dialog.setTitle(values[0]);
+            dialog.setMessage(values[1]);
+        }
+
+        private boolean sendData(List<Vector2D> vectorList) {
+            byte[] data = ToByteConverter.vectorListToByte(vectorList);
+            return BluetoothConn.send(data);
+        }
+
+        private AlertDialog createDialog(String title, String message) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder .setTitle(title)
+                    .setMessage(message);
+            return builder.create();
+        }
     }
 }
