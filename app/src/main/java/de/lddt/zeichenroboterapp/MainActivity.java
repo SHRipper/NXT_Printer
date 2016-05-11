49caf9b1ddd12d0c6cbc2a1cb1db9ea2a90837d3
Tim <vogelti64073@th-nuernberg.de>
2016-05-11 22:20:35 +0200

    change theme to light theme

--- app/src/main/java/de/lddt/zeichenroboterapp/MainActivity.java
+++ app/src/main/java/de/lddt/zeichenroboterapp/MainActivity.java
@@ -59,7 +59,6 @@ public class MainActivity extends Activity {
         }
 
         new TransferVectorsToBrick().execute(directionVectorList);
-
     }
 
     private void openSystemBluetoothSettings() {
