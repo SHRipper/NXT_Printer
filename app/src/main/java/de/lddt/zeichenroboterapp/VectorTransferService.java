e7419900a2a3a2a51ee604c723e00d362b1cd318
Tim <vogelti64073@th-nuernberg.de>
2016-05-21 02:15:59 +0200

    optimized imports

--- app/src/main/java/de/lddt/zeichenroboterapp/VectorTransferService.java
+++ app/src/main/java/de/lddt/zeichenroboterapp/VectorTransferService.java
@@ -1,9 +1,7 @@
 package de.lddt.zeichenroboterapp;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 import java.util.List;
 
