dc40865d173b02740b693718108fe9d8e56b15d9
TimBirdy <tim>
2016-05-25 20:34:44 +0200

    edited some comments

--- app/src/main/java/de/lddt/zeichenroboterapp/listener/TransferListener.java
+++ app/src/main/java/de/lddt/zeichenroboterapp/listener/TransferListener.java
@@ -4,10 +4,10 @@ package de.lddt.zeichenroboterapp.listener;
  * Listener interface for Actions while transferring vectors to the nxt brick.
  */
 public interface TransferListener {
-    //when trying to connect to the brick
+    //trying to connect to the brick
     void onConnect();
 
-    //when updating the amount of transferred packages
+    //updating the amount of transferred packages
     void onProgressUpdate(int progress, int packageCount);
 
     //successfully transferred the date to the brick
