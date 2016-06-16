75fe12bc60047375f382ce43bba719f5f47a6e49
TimBirdy <tim>
2016-06-16 21:58:17 +0200

    removed not needed code

--- app/src/main/java/de/lddt/zeichenroboterapp/math/Vector2D.java
+++ app/src/main/java/de/lddt/zeichenroboterapp/math/Vector2D.java
@@ -7,19 +7,9 @@ public class Vector2D {
     public float x;
     public float y;
 
-    public Vector2D() {
-        this.x = 0;
-        this.y = 0;
-    }
-
     public Vector2D(float x, float y) {
         this.x = x;
         this.y = y;
-    }
-
-    public void add(Vector2D v) {
-        this.x += v.x;
-        this.y += v.y;
     }
 
     /**
